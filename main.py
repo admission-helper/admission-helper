ea59de338bbf3cf593b701277c0e1441fb003db6
Pavivin <pavivin@yandex.ru>
2021-06-03 01:44:57 +0300

    hotfix

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from vkbottle.tools.dev_tools import keyboard
 from vkbottle.tools.dev_tools.keyboard.action import Payload
 from vkbottle.tools.dev_tools.mini_types.bot.message import message_min
 from config import TOKEN
+import uvloop
 
 bot = Bot(TOKEN)
 
@@ -51,6 +52,7 @@ async def subject_handler(message: Message) -> None:
     await message.answer(ANSWER_TEXT, keyboard=keyboard.get_json())
 
 @bot.on.message(payload={"subject": "back"})
+@bot.on.message(text=['Начать'])
 async def back_subject_handler(message: Message) -> None:
 
     KEYBOARD = (
@@ -62,11 +64,14 @@ async def back_subject_handler(message: Message) -> None:
 
     await message.answer('Я вас категорически приветствую', keyboard=KEYBOARD)
 
-@bot.on.message()
+@bot.on.message(payload={"subject": "next"})
 async def calc_handler(message: Message) -> None:
     # запрос сюда
-    specialties = []
-    await message.answer(f'Возможные специальности: {specialties}', keyboard=KEYBOARD)
+    available_specialties = []
+    subject_specialties = []
+    ANSWER_TEXT = f'''Специальности с вашими экзаменами: {subject_specialties}, 
+                      с подходящими баллами: {available_specialties}'''.strip()
+    await message.answer(ANSWER_TEXT, keyboard=KEYBOARD)
 
 
 @bot.on.message()
@@ -76,5 +81,14 @@ async def main_handler(message: Message) -> None:
         print({"exam": subject, "score": message.text, "id": message.id})
         # запрос сюда
         await subject_handler(message)
+    else:
+        KEYBOARD = (
+            Keyboard(one_time=True)
+            .add(Text("Калькулятор ЕГЭ"))
+            .add(Text("О боте"))
+            .get_json()
+        )
+        await message.answer('Доступные кнопки', keyboard=KEYBOARD)
 
+uvloop.install()
 bot.run_forever()
